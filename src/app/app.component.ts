36dc71e76b599d9ceaf53efd1e249cd1184ad862
Julio Saura <elcorreoesdejulio@gmail.com>
2024-11-14 19:25:31 +0100

    feat: add dynamic components iteration

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,20 +2,33 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ProfileSummaryComponent } from './components/profile-summary/profile-summary.component';
 import { NavigationComponent } from './components/navigation/navigation.component';
+import { OverviewComponent } from './components/sections/overview/overview.component';
+import { NgComponentOutlet } from '@angular/common';
+import { ProjectsComponent } from './components/sections/projects/projects.component';
+import { SkillsComponent } from './components/sections/skills/skills.component';
+import { ArticlesComponent } from './components/sections/articles/articles.component';
+import { ExperienceComponent } from './components/sections/experience/experience.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, ProfileSummaryComponent, NavigationComponent],
+  imports: [
+    RouterOutlet,
+    ProfileSummaryComponent,
+    NavigationComponent,
+    NgComponentOutlet,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'me';
+
   sections = [
-    { title: 'Sobre mi', id: 'overview' },
-    { title: 'Proyectos destacados', id: 'projects' },
-    { title: 'Habilidades', id: 'abilities' },
-    { title: 'Artículos', id: 'articles' },
+    { component: OverviewComponent, title: 'Sobre mí', id: 'overview' },
+    { component: ExperienceComponent, title: 'Experiencia', id: 'experience' },
+    { component: ProjectsComponent, title: 'Proyectos destacados', id: 'projects' },
+    { component: ArticlesComponent, title: 'Artículos', id: 'articles' },
+    { component: SkillsComponent, title: 'Habilidades', id: 'abilities' },
   ];
 }

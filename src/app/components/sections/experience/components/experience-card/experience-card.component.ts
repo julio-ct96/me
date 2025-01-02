cbdfe1b0b2e1fb6b0604c7c45a4a2d218ebd3809
Julio Saura <elcorreoesdejulio@gmail.com>
2025-01-02 09:40:24 +0100

    chore: remove standalone declaration because is default

--- src/app/components/sections/experience/components/experience-card/experience-card.component.ts
+++ src/app/components/sections/experience/components/experience-card/experience-card.component.ts
@@ -2,12 +2,9 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
 
 @Component({
   selector: 'app-experience-card',
-  standalone: true,
   imports: [],
   templateUrl: './experience-card.component.html',
   styleUrl: './experience-card.component.scss',
-  changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class ExperienceCardComponent {
-
-}
+export class ExperienceCardComponent {}

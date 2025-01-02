fa0a5ebe4d8fa36bfff79c191cd4cbb8e03b730b
Julio Saura <elcorreoesdejulio@gmail.com>
2025-01-02 09:06:44 +0100

    feat: add experience card component

---
+++ src/app/components/sections/experience/components/experience-card/experience-card.component.ts
@@ -0,0 +1,13 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-experience-card',
+  standalone: true,
+  imports: [],
+  templateUrl: './experience-card.component.html',
+  styleUrl: './experience-card.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class ExperienceCardComponent {
+
+}
